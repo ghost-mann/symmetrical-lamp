964cbf64b79c58084045884668c21c18d69ccd9e
ghost-mann <iamaob8@protonmail.com>
2025-04-26 11:43:06 +0300

    feat: added extractor class in main.py, added spotify.csv file

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ import requests
 import pandas as pd
 
 # extract data from csv file
-def from_csv(file_path: str):
-    return pd.read_csv(file_path)
+class Extractor:
+    @staticmethod
+    def from_csv(file_path: str):
+        # loads data from csv into df
+        return pd.read_csv(file_path)
+
+# stores resulting dataframe into variable df 
+# staticmethod - didnt need to create object - class instance
+df = Extractor.from_csv('spotify.csv')
+
+# data cleaning, aggregation, formatting
+class Transformer:
+
+    
+
+    
 
 

8f2767c041829dd37e618a333a6f94459b445a72
ghost-mann <iamaob8@protonmail.com>
2025-04-28 18:55:55 +0300

    feat: added transformer class in main.py

--- main.py
+++ main.py
@@ -14,6 +14,20 @@ df = Extractor.from_csv('spotify.csv')
 
 # data cleaning, aggregation, formatting
 class Transformer:
+    def __init__(self, df:pd.DataFrame):
+        self.df = df
+
+    def drop_null(self):
+        self.df = self.df.dropna()
+
+    def drop_duplicates(self):
+        self.df =  self.df.drop_duplicates()
+
+    def get_data(self):
+        return self.df
+
+
+
 
     
 
